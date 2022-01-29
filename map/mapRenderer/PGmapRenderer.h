2270fd1cdddf0667590dda5f4a618d898b54ccf2
StarSugar <92320877+StarSugar@users.noreply.github.com>
2022-01-29 20:50:50 +0800

    Update PGmapRenderer.h

--- map/mapRenderer/PGmapRenderer.h
+++ map/mapRenderer/PGmapRenderer.h
@@ -1,1 +1,20 @@
+//
+//  PGmap.h
+//  pixel_game
+//
+//  Created by gaoyuzhuo on 2022/1/28.
+//
 
+#ifndef PGrenderMap_h
+#define PGrenderMap_h
+
+#include <stdio.h>
+#include "PGtype.h"
+#include "PGtile.h"
+#include "PGerror.h"
+
+PGrenderMap PGcreatRenderMap(uint32_t height,uint32_t length);
+int PGloadRenderMap(char* filePath,unsigned int starH,unsigned int starL,unsigned int endH,unsigned int endL);
+int PGfreeRenderMap(PGrenderMap aMap);
+
+#endif /* PGmap_h */
