3e895f33014111d7a7d0c20a0ba903d9731fbf57
StarSugar <92320877+StarSugar@users.noreply.github.com>
2022-01-29 20:37:34 +0800

    Add files via upload

---
+++ PGloadSaves.h
@@ -0,0 +1,14 @@
+//
+//  PGloadSaves.h
+//  pixel_game
+//
+//  Created by gaoyuzhuo on 2022/1/28.
+//
+
+#ifndef PGloadSaves_h
+#define PGloadSaves_h
+
+#include <stdio.h>
+#include "PGtype.h"
+int PGloadSaves(char* filePath,PGloadSaveFile* var);
+#endif /* PGloadSaves_h */
