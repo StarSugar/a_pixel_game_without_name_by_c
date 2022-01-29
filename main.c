94cc74e06d94d26736d32037d5123a44f4a8d1f3
StarSugar <92320877+StarSugar@users.noreply.github.com>
2022-01-29 20:34:54 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  pixel_game
+//
+//  Created by gaoyuzhuo on 2022/1/27.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
